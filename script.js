783cfbf173027d12af398a194e461526115c634e
uswa.r <uswa.r@outlook.com>
2022-09-08 00:28:13 +0500

    added Join Method - One Way Data-Binding

--- script.js
+++ script.js
@@ -81,3 +81,19 @@ const menu = [{
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function() {
+    //now to iterate:
+    let displayMenu = menu.map(function(item) {
+        return `<article class="menu-item">
+                <img src="${item.img}" class="photo" alt="${item.title}">
+                <div class="item-info">
+                    <header>
+                        <h4>${item.title}</h4>
+                        <h4 class="price">${item.price}</h4>
+                    </header>
+                    <p class="item-text">${item.desc}</p>
+                </div>
+            </article>`
+    });
+});
