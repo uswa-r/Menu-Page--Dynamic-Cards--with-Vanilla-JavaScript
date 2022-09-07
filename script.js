a03c06167da533607a241222b280d90927cb2a25
uswa.r <uswa.r@outlook.com>
2022-09-08 00:50:34 +0500

    iterated the array this time

--- script.js
+++ script.js
@@ -83,8 +83,12 @@ const menu = [{
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function() {
-    //now to iterate:
-    let displayMenu = menu.map(function(item) {
+    displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems) {
+    //now to iterate the array instead of static database:
+    let displayMenu = menuItems.map(function(item) {
         return `<article class="menu-item">
                 <img src="${item.img}" class="photo" alt="${item.title}">
                 <div class="item-info">
@@ -98,4 +102,4 @@ window.addEventListener('DOMContentLoaded', function() {
     });
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
-});
+}
